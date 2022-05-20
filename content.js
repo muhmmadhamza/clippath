7e96816f222c7cc4db5d9ac535dfdc24dd72e782
muhmmadhamza <99123540+muhmmadhamza@users.noreply.github.com>
2022-05-20 17:24:04 +0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,18 @@
+// console.log("your are runnig in kittlen ");
+// // download(1).jfif;
+// // download(2).jfif;
+// // download(3).jfif;
+// // Kiss_Emoji_with_Closed_Eyes.webp;
+// let filenames = [
+//   "download(1).jpg",
+//   "download(2).jpg",
+//   "download(3).jpg",
+//   "Kiss_Emoji_with_Closed_Eyes.jpg",
+// ];
+// let imgs = document.getElementsByTagName("img");
+// for (imgElt of imgs) {
+//   let r = Math.floor(Math.random() * filenames.length);
+//   let file = "kittens/" + filenames[r];
+//   let url = chrome.extension.geturl(file);
+//   console.log(url);
+// }
---
+++ popup.js
@@ -0,0 +1,13 @@
+// let elements = document.getElementsByTagName("p");
+// console.log(`The number of p tags: ${elements.length}`);
+// let divsion = document.getElementsByTagName("div");
+// console.log(`The number of div tags: ${divsion.length}`);
+// let word = "online";
+// let arr = [];
+// document.querySelectorAll("p").forEach((item) => {
+//   if (item.innerText.includes(word)) {
+//     arr.push(item.innerText);
+//   }
+// });
+// console.log(arr.length, "Total Words");
+// console.log("running html");
